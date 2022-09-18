b3b37d2618926fd44497b6e8741e789e3ede8238
Elena Vasenina <lena-vasenina@mail.ru>
2022-09-18 16:52:05 -0400

    7 hw

--- Advanced course/js/main.js
+++ Advanced course/js/main.js
@@ -1,16 +1,16 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000, img: 'https://via.placeholder.com/200x150' },
+    { id: 2, title: 'Mouse', price: 20, img: 'https://via.placeholder.com/200x150' },
+    { id: 3, title: 'Keyboard', price: 200, img: 'https://via.placeholder.com/200x150' },
+    { id: 4, title: 'Gamepad', price: 50, img: 'https://via.placeholder.com/200x150' },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const img = document. createElement('img');
+const img = document.createElement('img');
 img.src = 'images/hvZpaUdW98M.jpg'
 const renderProduct = (product, img) => {
     return `<div class="product-item">
-                <img src='${img}'>
+                <img src='${product.img}'>
                 <h3>${product.title}</h3>
                 <p>${product.price}</p>
                 <button class="buy-btn">Купить</button>
